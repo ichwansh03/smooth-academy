2ab8c7bba9c626c4a4c4e3fcda3bd53f0076eb87
ichwansh03 <ichwansholihin70@gmail.com>
2026-07-15 20:45:01 +0700

    fully changes for finger math quiz asstes

--- src/utils/hands.js
+++ src/utils/hands.js
@@ -1,18 +1,6 @@
-function getHandsForNumber(num) {
-    if (num <= 0) return [{ fingersUp: 0 }];
-    if (num <= 5) return [{ fingersUp: num }];
-    const hands = [{ fingersUp: 5 }];
-    const rem = num - 5;
-    if (rem > 0) hands.push({ fingersUp: rem });
-    return hands;
-}
-
 export function renderHandsForNumber(num) {
-    const hands = getHandsForNumber(num);
-    let html = '<div class="hand-group">';
-    hands.forEach(function (h) {
-        html += '<img class="hand-svg" src="/assets/images/hand-' + h.fingersUp + '.svg" alt="' + h.fingersUp + ' finger' + (h.fingersUp !== 1 ? 's' : '') + '">';
-    });
-    html += '</div>';
-    return html;
+    if (num >= 0 && num <= 9) {
+        return '<img class="hand-svg" src="/assets/images/' + num + '.svg" alt="' + num + '">';
+    }
+    return '<span class="number-text">' + num + '</span>';
 }
