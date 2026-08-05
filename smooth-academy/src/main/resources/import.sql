48a6279abca6f36729056db029ca53a21a10a27d
ichwansh03 <ichwansholihin70@gmail.com>
2026-08-05 20:18:35 +0700

    change question for one stars

--- smooth-academy/src/main/resources/import.sql
+++ smooth-academy/src/main/resources/import.sql
@@ -1,5 +1,5 @@
 INSERT INTO levels (id, name, icon, label, min_range, max_range, required_stars, sort_order) VALUES
-(1, 'Ones Star', '🌟', '1–9', 1, 9, 0, 1),
-(2, 'Tens Star', '⭐🌟', '10–99', 10, 99, 3, 2),
-(3, 'Hundreds Star', '💫', '100–999', 100, 999, 3, 3),
-(4, 'Thousands Star', '✨', '1000–9999', 1000, 9999, 3, 4);
+(1, 'Satuan', '🌟', '1–9', 1, 9, 0, 1),
+(2, 'Puluhan', '⭐🌟', '10–99', 10, 99, 3, 2),
+(3, 'Ratusan', '💫', '100–999', 100, 999, 3, 3),
+(4, 'Ribuan', '✨', '1000–9999', 1000, 9999, 3, 4);
